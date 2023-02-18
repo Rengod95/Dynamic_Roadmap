54357147b53ac3bd7eb2af09fae5b0b472965e44
imnotmoon <imnotmoon@gmail.com>
2023-02-19 00:56:58 +0900

    feat: catch error with message

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,3 +1,10 @@
 import { FetchOptions } from '@tanstack/query-core/build/lib/query';
+import { BaseError } from '@/utils/error';
 
-export const request = (args: Parameters<typeof fetch>) => fetch(...args).then((res) => res.json());
+export const request = (args: Parameters<typeof fetch>) =>
+  fetch(...args)
+    .then((res) => res.json())
+    .catch((error: Error) => {
+      const { message } = error;
+      throw new BaseError(message);
+    });
