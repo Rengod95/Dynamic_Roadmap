6ce95667d7952d1f068e5aa1a0c4b4f2820796db
imnotmoon <imnotmoon@gmail.com>
2023-02-19 01:30:49 +0900

    fix: fix fetch interface

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,8 +1,8 @@
 import { FetchOptions } from '@tanstack/query-core/build/lib/query';
 import { BaseError } from '@/utils/error';
 
-export const request = (args: Parameters<typeof fetch>) =>
-  fetch(...args)
+export const request = <TResponse>(url: string, config: RequestInit = {}): Promise<TResponse> =>
+  fetch(url, config)
     .then((res) => res.json())
     .catch((error: Error) => {
       const { message } = error;
